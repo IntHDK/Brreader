4912866f1654b4ba122a2b47b04a30f9943b59d9
IntHDK <kimhy1198@gmail.com>
2016-11-10 17:58:23 +0900

    Rebuilded Library

---
+++ app/src/main/java/kr/co/momtalk/momtalk/ImageProcessor.java
@@ -0,0 +1,14 @@
+package kr.co.momtalk.momtalk;
+import org.opencv.*;
+/**
+ * Created by kimhy on 2016-11-08.
+ */
+
+public class ImageProcessor {
+    private boolean[][] points;
+    private String intepreted;
+
+    public void parseimage(){
+
+    }
+}
--- openCVLibrary310/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary310/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\kimhy\\Brreader\\openCVLibrary310\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\kimhy\\GitHubRepo\\Brreader\\openCVLibrary310\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
